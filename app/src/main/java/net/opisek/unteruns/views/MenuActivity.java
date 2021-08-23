0b6cd0fa080986188c2ad8a01fd172c91788696e
Opisek <darowski.kacper@gmail.com>
2021-08-23 17:08:40 +0200

    strings in strings.xml

--- app/src/main/java/net/opisek/unteruns/views/MenuActivity.java
+++ app/src/main/java/net/opisek/unteruns/views/MenuActivity.java
@@ -1,5 +1,27 @@
 package net.opisek.unteruns.views;
 
-public class MenuActivity {
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import net.opisek.unteruns.R;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
 }
